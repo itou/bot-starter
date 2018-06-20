140017d9e7106db237f37a2c8c50329927e36837
Masahiro Ito <itou@tdc-ict.co.jp>
2018-06-20 21:22:52 +0900

    ユーザーとメッセージをデータベースに登録する

--- index.php
+++ index.php
@@ -26,18 +26,33 @@ try {
 
 // 配列に格納された各イベントをループで処理
 foreach ($events as $event) {
+
   // MessageEventクラスのインスタンスでなければ処理をスキップ
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent)) {
     error_log('Non message event has come');
     continue;
   }
+
   // TextMessageクラスのインスタンスでなければ処理をスキップ
   if (!($event instanceof \LINE\LINEBot\Event\MessageEvent\TextMessage)) {
     error_log('Non text message has come');
     continue;
   }
+
+  // ユーザーをデータベースに登録
+  registerUser($event->getUserId(), $event->getText());
+
   // オウム返し
   $bot->replyText($event->getReplyToken(), $event->getText());
+
+}
+
+// ユーザーをデータベースに登録する
+function registerUser($userId, $text) {
+  $dbh = dbConnection::getConnection();
+  $sql = 'insert into textMessage (userid, message) values (pgp_sym_encrypt(?, \'' . getenv('DB_ENCRYPT_PASS') . '\'), ?) ';
+  $sth = $dbh->prepare($sql);
+  $sth->execute(array($userId, $text));
 }
 
 // テキストを返信。引数はLINEBot、返信先、テキスト
@@ -167,6 +182,37 @@ function replyCarouselTemplate($bot, $replyToken, $alternativeText, $columnArray
   $response = $bot->replyMessage($replyToken, $builder);
   if (!$response->isSucceeded()) {
     error_log('Failed!'. $response->getHTTPStatus . ' ' . $response->getRawBody());
+  }
+}
+
+// データベースへの接続を管理するクラス
+class dbConnection {
+  // インスタンス
+  protected static $db;
+  // コンストラクタ
+  private function __construct() {
+
+    try {
+      // 環境変数からデータベースへの接続情報を取得し
+      $url = parse_url(getenv('DATABASE_URL'));
+      // データソース
+      $dsn = sprintf('pgsql:host=%s;dbname=%s', $url['host'], substr($url['path'], 1));
+      // 接続を確立
+      self::$db = new PDO($dsn, $url['user'], $url['pass']);
+      // エラー時例外を投げるように設定
+      self::$db->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
+    }
+    catch (PDOException $e) {
+      error_log('Connection Error: ' . $e->getMessage());
+    }
+  }
+
+  // シングルトン。存在しない場合のみインスタンス化
+  public static function getConnection() {
+    if (!self::$db) {
+      new dbConnection();
+    }
+    return self::$db;
   }
 }
 
